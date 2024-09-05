1fafa812399a59cec049af47206101040f173d80
Hítalo Rodrigues <hitalo005@gmail.com>
2024-09-05 13:53:17 -0300

    feat: created new App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,85 @@
-import styles from './AppStyles.module.css'
-import { Header } from './components/header/header'
-import { Tasks } from './components/tasks/tasks'
 import './global.css'
 
+import { useReducer } from 'react'
+
+import AddTask from './components/addTask/AddTask'
+import { Header } from './components/header/header'
+import TaskList from './components/taskList/TaskList'
+import { Tasks } from './components/tasks/tasks'
+
+interface Task {
+  id: number
+  text: string
+  completed: boolean
+}
+
+function tasksReducer(tasks, action) {
+  switch (action.type) {
+    case 'added': {
+      return [
+        ...tasks,
+        {
+          id: action.id,
+          text: action.text,
+          completed: false,
+        },
+      ]
+    }
+    case 'changed': {
+      return tasks.map((task) => {
+        if (task.id === action.task.id) {
+          return action.task
+        } else {
+          return task
+        }
+      })
+    }
+    case 'deleted': {
+      return tasks.filter((task) => task.id !== action.id)
+    }
+    default: {
+      throw Error('Unknown action: ' + action.type)
+    }
+  }
+}
+
 export function App() {
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks)
+
+  function handleAddTask(text: string) {
+    dispatch({
+      type: 'added',
+      id: nextId++,
+      text,
+    })
+  }
+
+  function handleChangeTask(task) {
+    dispatch({
+      type: 'changed',
+      task,
+    })
+  }
+
+  function handleDeleteTask(taskId) {
+    dispatch({
+      type: 'deleted',
+      id: taskId,
+    })
+  }
 
   return (
-    <main className={styles.main}>
+    <main className="flex flex-col items-center gap-8">
       <Header />
-      <Tasks />
+      <AddTask onAddTask={handleAddTask} />
+      <TaskList
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeleteTask={handleDeleteTask}
+      />
     </main>
   )
 }
+
+let nextId = 0
+const initialTasks: Task[] = []
