0e62e1c119bd884ed02942fef0c0cbdce7106838
Hítalo Rodrigues <hitalo005@gmail.com>
2024-09-05 15:39:02 -0300

    refactor: changed header tasklist

--- src/components/taskList/TaskList.tsx
+++ src/components/taskList/TaskList.tsx
@@ -6,23 +6,25 @@ import { NoTask } from '../notask/notask'
 export default function TaskList({ tasks, onChangeTask, onDeleteTask }) {
   return (
     <>
+      <header className="flex w-[46rem] flex-row justify-between gap-2">
+        <section className="text-blue1 flex items-center gap-2 text-base font-bold">
+          <p>Tarefas criadas</p>
+          <p className="text-gray2 bg-gray4 flex items-center rounded-full pl-2 pr-2">
+            {tasks.length}
+          </p>
+        </section>
+        <section className="text-purple flex items-center gap-2 text-base font-bold">
+          <p>Concluídas</p>
+          <p className="text-gray2 bg-gray4 flex items-center rounded-full pl-2 pr-2">
+            {tasks.length === 0 && <span>0</span>}
+            {tasks.length !== 0 && <span>0 de {tasks.length}</span>}
+          </p>
+        </section>
+      </header>
+      <div className="bg-gray4 h-[0.5px] w-[46rem]" />
       {tasks.length === 0 && <NoTask />}
       {tasks.length !== 0 && (
         <div className="flex w-[44rem] flex-col items-center justify-center gap-16 pb-4 pt-4">
-          <header className="mb-6 flex w-[46rem] flex-row justify-between gap-2">
-            <section className="text-blue1 flex items-center gap-2 text-base font-bold">
-              <p>Tarefas criadas</p>
-              <p className="text-gray2 bg-gray4 flex items-center rounded-full pb-1 pl-2 pr-2 pt-1">
-                {tasks.length}
-              </p>
-            </section>
-            <section className="text-purple flex items-center gap-2 text-base font-bold">
-              <p>Concluídas</p>
-              <p className="text-gray2 bg-gray4 flex items-center rounded-full pb-1 pl-2 pr-2 pt-1">
-                0 de {tasks.length}
-              </p>
-            </section>
-          </header>
           <ul>
             {tasks.map((task) => (
               <li key={task.id}>
