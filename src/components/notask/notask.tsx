dbc90d093a5ef7340678f9df16816d349ddd99e7
Hítalo Rodrigues <hitalo005@gmail.com>
2024-09-05 15:37:56 -0300

    refactor style: changed css to tailwindcss

--- src/components/notask/notask.tsx
+++ src/components/notask/notask.tsx
@@ -1,19 +1,17 @@
-import styles from './notaskStyles.module.css'
 import agenda from '../../assets/Clipboard.png'
-
 
 export function NoTask() {
   return (
-    <>
+    <div className="mt-12 flex flex-col items-center gap-4">
       <img src={agenda} alt="" />
       <span>
-        <p className={styles.text1}>
+        <p className="text-gray3 text-center text-base font-bold">
           Você ainda não tem tarefas cadastradas
         </p>
-        <p className={styles.text2}>
+        <p className="text-gray3 text-center text-base">
           Crie tarefas e organize seus itens a fazer
         </p>
       </span>
-    </>
+    </div>
   )
 }
